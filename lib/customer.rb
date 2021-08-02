6489abecd43d632c96a307511819c9d40d4d9bde
Asim Ali <asimsali95@gmail.com>
2021-08-02 14:15:58 -0500

    commit tree e3956c54f933afc4bbab37e1546901637529a8d7

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,32 @@
 class Customer
+    attr_accessor :name, :age 
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+      end
+    
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip=0)#no need for an argument for customer/self?
+        Meal.new(waiter, self, total, tip)#self where customer is needed
+    end
+
+    def meals 
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end
+
+    def waiters
+        meals.map do |meal|
+          meal.waiter
+        end
+    end
   
 end
--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,17 @@
 class Meal
+    attr_accessor :waiter, :customer, :total, :tip #waiter and customer shared across all 3 classes
 
+    @@all = []
+  
+    def initialize(waiter, customer, total, tip=0)
+      @waiter = waiter
+      @customer = customer
+      @total = total
+      @tip = tip
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
 end
